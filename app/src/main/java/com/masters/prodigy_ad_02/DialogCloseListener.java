726f2ea9a0615f0374cec2633793321764cd4b44
PriyanshuRauth <raut.priyanshu30@gmail.com>
2024-10-05 09:42:04 +0530

    Initial commit

---
+++ app/src/main/java/com/masters/prodigy_ad_02/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.masters.prodigy_ad_02;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
